4b1ceff0d7a53a8f14a5401967b760af0d826f22
Erik Faye-Lund <erik.faye-lund@collabora.com>
2022-05-04 10:06:53 +0200

    gitlab-ci: move glut to /tmp
    Due to some boring details, Meson doesn't like if we specify library and
    include paths to compiler.find_library inside the source dir like we did
    with CMake. So let's prepare the GLUT stuff in /tmp instead.
    
    Acked-by: Jose Fonseca <jfonseca@vmware.com>
    Acked-by: Eric Engestrom <eric@engestrom.ch>

--- .gitlab-ci/build-mingw.sh
+++ .gitlab-ci/build-mingw.sh
@@ -7,8 +7,8 @@ fi
 
 set -e -x -u
 
-GLUT_INCLUDE_DIR="$PWD/external/freeglut/include"
-GLUT_LIBRARY="$PWD/external/freeglut/lib/libglut.a"
+GLUT_INCLUDE_DIR="/tmp/freeglut-mingw/include"
+GLUT_LIBRARY="/tmp/freeglut-mingw/lib/libglut.a"
 if [ ! -f "$GLUT_INCLUDE_DIR/GL/glut.h" ]
 then
 	mkdir -p "$GLUT_INCLUDE_DIR/GL"
