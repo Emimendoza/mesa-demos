1afb829bd5ddbce7da0f011bdd53902bdf670586
Erik Faye-Lund <erik.faye-lund@collabora.com>
2022-12-14 10:33:17 +0100

    eglut_wayland: handle keyboard-input
    This uses wl_seat + xkbcommon to handle keyboard input.
    
    Currently lacks support for key-repeat and reporting keys pressed while
    window was inactive.
    
    Reviewed-by: Hoe Hao Cheng <haochengho12907@gmail.com>

--- src/egl/eglut/eglut_wayland.c
+++ src/egl/eglut/eglut_wayland.c
@@ -1,19 +1,31 @@
 #include <wayland-client.h>
 #include <wayland-egl.h>
 
+#include <assert.h>
 #include <poll.h>
 #include <errno.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <xkbcommon/xkbcommon.h>
 
 #include "eglutint.h"
 #include "wayland-xdg-shell-client-protocol.h"
 
 struct display {
    struct wl_display *display;
+   struct wl_seat *wl_seat;
    struct wl_compositor *compositor;
    struct xdg_wm_base *xdg_wm_base;
    uint32_t mask;
+
+   struct {
+      struct wl_keyboard *keyboard;
+      struct xkb_state *xkb_state;
+      struct xkb_context *xkb_context;
+      struct xkb_keymap *xkb_keymap;
+   } seat;
 };
 
 struct window {
@@ -38,6 +50,159 @@ static const struct xdg_wm_base_listener xdg_wm_base_listener = {
 };
 
 static void
+keymap_callback(void *data, struct wl_keyboard *wl_keyboard,
+                uint32_t format, int32_t fd, uint32_t size)
+{
+   struct display *d = data;
+   assert(format == WL_KEYBOARD_KEYMAP_FORMAT_XKB_V1);
+
+   char *map_shm = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+   assert(map_shm != MAP_FAILED);
+
+   struct xkb_keymap *xkb_keymap = xkb_keymap_new_from_string(
+      d->seat.xkb_context, map_shm, XKB_KEYMAP_FORMAT_TEXT_V1,
+      XKB_KEYMAP_COMPILE_NO_FLAGS);
+   munmap(map_shm, size);
+   close(fd);
+
+   struct xkb_state *xkb_state = xkb_state_new(xkb_keymap);
+   xkb_keymap_unref(d->seat.xkb_keymap);
+   xkb_state_unref(d->seat.xkb_state);
+   d->seat.xkb_keymap = xkb_keymap;
+   d->seat.xkb_state = xkb_state;
+}
+
+
+static int
+lookup_keysym(xkb_keysym_t sym)
+{
+   switch (sym) {
+
+   /* function keys */
+   case XKB_KEY_F1: return EGLUT_KEY_F1;
+   case XKB_KEY_F2: return EGLUT_KEY_F2;
+   case XKB_KEY_F3: return EGLUT_KEY_F3;
+   case XKB_KEY_F4: return EGLUT_KEY_F4;
+   case XKB_KEY_F5: return EGLUT_KEY_F5;
+   case XKB_KEY_F6: return EGLUT_KEY_F6;
+   case XKB_KEY_F7: return EGLUT_KEY_F7;
+   case XKB_KEY_F8: return EGLUT_KEY_F8;
+   case XKB_KEY_F9: return EGLUT_KEY_F9;
+   case XKB_KEY_F10: return EGLUT_KEY_F10;
+   case XKB_KEY_F11: return EGLUT_KEY_F11;
+   case XKB_KEY_F12: return EGLUT_KEY_F12;
+
+   /* directional keys */
+
+   case XKB_KEY_Left:
+   case XKB_KEY_KP_Left:
+      return EGLUT_KEY_LEFT;
+
+   case XKB_KEY_Up:
+   case XKB_KEY_KP_Up:
+      return EGLUT_KEY_UP;
+
+   case XKB_KEY_Right:
+   case XKB_KEY_KP_Right:
+      return EGLUT_KEY_RIGHT;
+
+   case XKB_KEY_Down:
+   case XKB_KEY_KP_Down:
+      return EGLUT_KEY_DOWN;
+
+   default:
+      return -1;
+   }
+}
+
+static void
+emit_keypress(struct display *d, xkb_keycode_t keycode)
+{
+   struct eglut_window *win = _eglut->current;
+
+   xkb_keysym_t sym = xkb_state_key_get_one_sym(d->seat.xkb_state, keycode);
+   int special = lookup_keysym(sym);
+   if (special >= 0)
+      win->special_cb(special);
+   else {
+      uint32_t utf32 = xkb_keysym_to_utf32(sym);
+      if (utf32 && utf32 < 128)
+         win->keyboard_cb(utf32);
+   }
+}
+
+static void
+handle_key(struct display *d, xkb_keycode_t keycode,
+           enum wl_keyboard_key_state state)
+{
+   if (state == WL_KEYBOARD_KEY_STATE_PRESSED)
+      emit_keypress(d, keycode);
+}
+
+static void
+enter_callback(void *data, struct wl_keyboard *wl_keyboard,
+               uint32_t serial, struct wl_surface *surface,
+               struct wl_array *keys)
+{
+   struct display *d = data;
+   uint32_t *key;
+   wl_array_for_each(key, keys)
+      handle_key(d, *key + 8, WL_KEYBOARD_KEY_STATE_PRESSED);
+}
+
+static void
+leave_callback(void *data, struct wl_keyboard *wl_keyboard,
+               uint32_t serial, struct wl_surface *surface)
+{
+}
+
+static void
+key_callback(void *data, struct wl_keyboard *keyboard,
+             uint32_t serial, uint32_t time, uint32_t key,
+             uint32_t state)
+{
+   struct display *d = data;
+   handle_key(d, key + 8, state);
+}
+
+static void
+modifiers_callback(void *data, struct wl_keyboard *wl_keyboard,
+                   uint32_t serial, uint32_t mods_depressed,
+                   uint32_t mods_latched, uint32_t mods_locked,
+                   uint32_t group)
+{
+   struct display *d = data;
+   xkb_state_update_mask(d->seat.xkb_state, mods_depressed, mods_latched,
+                         mods_locked, 0, 0, group);
+}
+
+static const struct wl_keyboard_listener keyboard_listener = {
+   keymap_callback,
+   enter_callback,
+   leave_callback,
+   key_callback,
+   modifiers_callback,
+};
+
+static void
+seat_capabilities(void *data, struct wl_seat *seat,
+                  enum wl_seat_capability caps)
+{
+   struct display *d = data;
+   if (caps & WL_SEAT_CAPABILITY_KEYBOARD) {
+      d->seat.keyboard = wl_seat_get_keyboard(seat);
+      wl_keyboard_add_listener(d->seat.keyboard, &keyboard_listener, data);
+   } else if (!(caps & WL_SEAT_CAPABILITY_KEYBOARD)) {
+      wl_keyboard_destroy(d->seat.keyboard);
+      d->seat.keyboard = NULL;
+   }
+}
+
+static const struct wl_seat_listener seat_listener = {
+   seat_capabilities,
+};
+
+static void
 registry_handle_global(void *data, struct wl_registry *registry, uint32_t id,
                        const char *interface, uint32_t version)
 {
@@ -50,6 +215,10 @@ registry_handle_global(void *data, struct wl_registry *registry, uint32_t id,
       d->xdg_wm_base =
          wl_registry_bind(registry, id, &xdg_wm_base_interface, 1);
       xdg_wm_base_add_listener(d->xdg_wm_base, &xdg_wm_base_listener, d);
+   } else if (strcmp(interface, wl_seat_interface.name) == 0) {
+      d->wl_seat =
+         wl_registry_bind(registry, id, &wl_seat_interface, 1);
+      wl_seat_add_listener(d->wl_seat, &seat_listener, d);
    }
 }
 
@@ -104,6 +273,8 @@ _eglutNativeInitDisplay(void)
    if (!_eglut->native_dpy)
       _eglutFatal("failed to initialize native display");
 
+   display.seat.xkb_context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
+
    registry = wl_display_get_registry(_eglut->native_dpy);
    wl_registry_add_listener(registry, &registry_listener, &display);
    wayland_roundtrip(_eglut->native_dpy);
@@ -119,6 +290,8 @@ _eglutNativeInitDisplay(void)
 void
 _eglutNativeFiniDisplay(void)
 {
+   wl_seat_destroy(display.wl_seat);
+   xkb_context_unref(display.seat.xkb_context);
    xdg_wm_base_destroy(display.xdg_wm_base);
    wl_compositor_destroy(display.compositor);
    wl_display_flush(_eglut->native_dpy);
