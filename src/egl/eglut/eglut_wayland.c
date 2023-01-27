Emimendoza/mesa-demos

commit 12ff8b7b42aaa52b6436c0cde0ae8cdf71ee8e98

eglut_wayland: do not use frame-listener
Even the simple-egl.c example client from Weston hasn't been using a
frame-listener since some time in 2013, and the way we used it causes
issues with the FPS counter, due to not keeping multiple frames in
flight.

Let's drop using the frame-listener, and simplify the code a bit.

Closes: https://gitlab.freedesktop.org/mesa/demos/-/issues/12

src/egl/eglut/eglut_wayland.c
@@ -20,7 +20,6 @@ struct window {
    struct wl_surface *surface;
    struct xdg_surface *xdg_surface;
    struct xdg_toplevel *xdg_toplevel;
-   struct wl_callback *callback;
    bool opaque;
    bool configured;
 };
@@ -215,28 +214,12 @@ _eglutNativeFiniWindow(struct eglut_window *win)
 
    xdg_toplevel_destroy(window.xdg_toplevel);
    xdg_surface_destroy(window.xdg_surface);
-
-   if (window.callback)
-      wl_callback_destroy(window.callback);
 }
 
 static void
-draw(void *data, struct wl_callback *callback, uint32_t time);
-
-static const struct wl_callback_listener frame_listener = {
-   draw
-};
-
-static void
-draw(void *data, struct wl_callback *callback, uint32_t time)
+draw(struct window *window)
 {
-   struct window *window = (struct window *)data;
    struct eglut_window *win = _eglut->current;
-
-   if (callback) {
-      wl_callback_destroy(callback);
-      window->callback = NULL;
-   }
 
    /* Our client doesn't want to push another frame; go back to sleep. */
    if (!_eglut->redisplay)
@@ -245,9 +228,6 @@ draw(void *data, struct wl_callback *callback, uint32_t time)
 
    if (win->display_cb)
       win->display_cb();
-
-   window->callback = wl_surface_frame(window->surface);
-   wl_callback_add_listener(window->callback, &frame_listener, window);
 
    if (window->opaque) {
       struct wl_region *region =
@@ -284,8 +264,8 @@ _eglutNativeEventLoop(void)
 
          /* Client wants to redraw, but we have no frame event to trigger the
           * redraw; kickstart it by redrawing immediately. */
-         if (_eglut->redisplay && !window.callback)
-            draw(&window, NULL, 0);
+         if (_eglut->redisplay)
+            draw(&window);
       }
 
       ret = wl_display_flush(display.display);

commit f87eeb37cb4792517333fe42a2973f5325e72c72

eglut_wayland: terminate mainloop on display-close
When we're closing the native display, we can't really keep processing
events. Let's terminate the mainloop to prevent crashes.

It's not clear to me why this used to work, but it seems like *somehow*
the frame-callback saved us.

Fixes: 12ff8b7b ("eglut_wayland: do not use frame-listener")
Closes: https://gitlab.freedesktop.org/mesa/demos/-/issues/28
Reviewed-by: Hoe Hao Cheng <haochengho12907@gmail.com>

src/egl/eglut/eglut_wayland.c
@@ -123,6 +123,7 @@ _eglutNativeFiniDisplay(void)
    wl_compositor_destroy(display.compositor);
    wl_display_flush(_eglut->native_dpy);
    wl_display_disconnect(_eglut->native_dpy);
+   _eglut->native_dpy = NULL;
 }
 
 static void
@@ -253,7 +254,7 @@ _eglutNativeEventLoop(void)
    while (!window.configured)
       wl_display_dispatch(display.display);
 
-   while (1) {
+   while (_eglut->native_dpy != NULL) {
       /* If we need to flush but can't, don't do anything at all which could
        * push further events into the socket. */
       if (!(pollfd.events & POLLOUT)) {