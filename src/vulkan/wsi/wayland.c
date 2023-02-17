a790f247ce80c4c81b4777a14086cf7a8edc64db
Hoe Hao Cheng <haochengho12907@gmail.com>
2023-02-17 22:28:21 +0800

    vulkan/wsi/wayland: fix clang warnings on missing field initializers
    Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>

--- src/vulkan/wsi/wayland.c
+++ src/vulkan/wsi/wayland.c
@@ -264,7 +264,7 @@ libdecor_error(struct libdecor *context,
 }
 
 static struct libdecor_interface libdecor_interface = {
-   libdecor_error,
+   .error = libdecor_error,
 };
 
 static void
@@ -309,9 +309,9 @@ frame_commit(struct libdecor_frame *frame, void *user_data)
 }
 
 static struct libdecor_frame_interface frame_interface = {
-   frame_configure,
-   frame_close,
-   frame_commit,
+   .configure = frame_configure,
+   .close = frame_close,
+   .commit = frame_commit,
 };
 
 static void init_window(const char *title, int width, int height, bool fullscreen)
